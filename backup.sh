7ab18b4bfa22d1d8f1c2a57f49fe5b140a556133
Cloud User <Cloud User centos@ip-172-31-12-6.ap-southeast-1.compute.internal>
2017-03-13 15:41:17 +0000

    automatic commit

--- backup.sh
+++ backup.sh
@@ -1,2 +1,3 @@
 docker cp 4e2c7fe12cab:/var/dokuwiki-storage/ /home/centos/wiki/; 
+git --git-dir /home/centos/wiki/.git --work-tree  /home/centos/wiki/ add . ;
 git --git-dir /home/centos/wiki/.git --work-tree  /home/centos/wiki/ commit -a -m "automatic commit";
---
+++ dokuwiki-storage/conf/users.auth.php
@@ -0,0 +1,15 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+helena:$1$tZL0zTih$NHk67Fc15DMpnLDCxhksz1:Helen Nevskaya:helen.nevskaya@linkit360.com:admin,user
+kirill:$1$6dcU6Cz5$s60wE0EpKL3GBBh2dksoG0:Kirill Goryunov:kirill.goryunov@linkit360.com:admin,user
+caravus:$1$HeGdtPQ4$xfUn0ENSa8KbFiZWhOpmN0:Evgeniy Denisenko:evgeniy.denisenko@linkit360.com:user
+vostrok:$1$z5Bi9Wke$pEJIg5Vv0Wqr6oSIizY5e1:vostrok:vostrok@linkit360.com:admin,user
