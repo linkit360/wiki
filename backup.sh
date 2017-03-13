97682a6ab664ee71b27e6412e39dcc4e7db4563d
Cloud User <Cloud User centos@ip-172-31-12-6.ap-southeast-1.compute.internal>
2017-03-13 15:47:41 +0000

    automatic commit

--- backup.sh
+++ backup.sh
@@ -1,3 +1,4 @@
 docker cp 4e2c7fe12cab:/var/dokuwiki-storage/ /home/centos/wiki/; 
 git --git-dir /home/centos/wiki/.git --work-tree  /home/centos/wiki/ add . ;
 git --git-dir /home/centos/wiki/.git --work-tree  /home/centos/wiki/ commit -a -m "automatic commit";
+git --git-dir /home/centos/wiki/.git --work-tree  /home/centos/wiki/ push -u origin master --quiet;
